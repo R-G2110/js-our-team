1908b3f814a7a34d6309cb220f01856f5896c4ea
R-G2110 <argeldelacruz86@gmail.com>
2023-09-21 22:33:47 +0200

    [Add]: team members card from script.js

--- js/script.js
+++ js/script.js
@@ -39,5 +39,22 @@ for( let member of team){
   console.log('Member Name: '+member.name);
   console.log('Member Role: '+member.role);
   console.log('Member Photo: '+member.photo);
-  console.log('============================================');
+  console.log('====================================================');
+}
+
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+//MILESTONE 2
+const card = document.querySelector('.team-members');
+for( let i = 0; i < team.length; i++){
+  card.innerHTML += `<div class="card card-member col-4 mx-1 my-1">
+                      <div class="card-body">
+                        <div class="images">
+                          <img src="img/${team[i].photo}" alt="${team[i].name}">
+                        </div>
+                        <div class="card-text text-center  my-3 ">
+                          <h4 class="member-name">${team[i].name}</h4>
+                          <span class="member-role">${team[i].role}</span>
+                        </div>
+                      </div>
+                    </div>`
 }
